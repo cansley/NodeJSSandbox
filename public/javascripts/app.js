a3458126b0f617497a6f2f056e8bd3ab022112c8
cansley <cansley2@gmail.com>
2014-11-17 13:21:38 -0600

    updating TODO to use angular.
    Changing the todo.html and associated js files to use app.js and
    angular. Also adding todo controller to app.js as well as other
    formatting changes to pretty it up a bit.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -20,8 +20,39 @@ jetbrains.controller("AppCtrl", function($http){
             loadProducts();
         })
 
-    }
+    };
 
 
     loadProducts();
+});
+
+jetbrains.controller("TodoCtrl", function($scope, $http){
+    var app = this;
+    var url = "http://localhost:3000/todo";
+
+
+    function loadTodos(){
+        $http.get(url).then(function(todos){
+            app.todos = todos.data.items;
+            app.newTodo = "";
+        }, function(err){
+            console.log(err.message);
+        });
+    }
+
+    app.saveTodo = function(newTodo){
+        $http.post(url, {newItem: newTodo})
+            .success(function() {
+                loadTodos();
+            });
+    };
+
+    app.removeTodo = function(todo){
+        $http.delete(url+"/"+todo.idx)
+            .success(function(){
+                loadTodos();
+            });
+    };
+
+    loadTodos();
 });
